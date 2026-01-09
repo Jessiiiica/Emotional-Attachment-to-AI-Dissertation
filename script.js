9a2bdf3d1a1250b659737c35109328c025906627
Jessica Petherbridge <jp2049@hw.ac.uk>
2026-01-09 15:32:08 +0000

    Added in timer for chatbot interaction pages (including: fixed bug for linking in script to html, added in banners hidden and new data varaible to html for this, created js that times how long the window has been opened for and shows / removes banners as necessary and at the end of 10mins the user is automatically moved to the next page)

---
+++ script.js
@@ -0,0 +1,55 @@
+/*Chatbot interaction timer so each user only spends exactly 10mins talking to each chatbot*/
+window.addEventListener("DOMContentLoaded", () => {
+    //Added in a data variable so only two of our pages (chabotA and chatbotB) will have this, this is added in the <body> in html
+    if (!document.body.dataset.tenMins) return;
+    const banner9mins = document.getElementById("banner9mins");
+    const banner10secs = document.getElementById("banner10secs");
+    let shown9mins = false;
+    let shown10sec = false;
+
+    //start our timer on page load
+    const start = Date.now();
+
+    //function to show the banner only for 5 seconds once it is told to appear
+    function showOnly5Seconds(el) {
+        //if the given banner doesnt exist then don't try anything as this would cause a crash
+        if (!el) {
+            return;
+        }
+        else {
+            el.classList.remove("hidden");
+            el.classList.add("show");
+
+            setTimeout(() => {
+                el.classList.remove("show")
+                setTimeout(() => el.classList.add("hidden"), 600);
+            }, 5000);
+        }
+    }
+
+    //check if either of the banners are needed (based on time via ticks) and if they are show them
+    const tick = () => {
+        const timeGone = Date.now() - start;
+        const timeLeft = 600000 - timeGone;
+
+        //when one min left (60,000) and it hasnt been shown before, show the banner
+        if (!shown9mins && timeLeft <= 60000) {
+            shown9mins = true;
+            showOnly5Seconds(banner9mins);
+        }
+
+        //when 10 seconds left (10,000) and it hasnt been shown before, show the banner
+        if (!shown10sec && timeLeft <= 10000) {
+            shown10sec = true;
+            showOnly5Seconds(banner10secs);
+        }
+
+        //if 10mins have passed then redirect the user to the next page
+        if (timeLeft <= 0) {
+            window.location.assign("chatbotAForm.html");
+            return;
+        }
+        requestAnimationFrame(tick);
+        }; 
+    tick();
+});
